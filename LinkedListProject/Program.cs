21df22cab61e2e66aacd7c3df1f00ecf54ee8aac
N1vas <ggj.nivas.18@gmail.com>
2020-10-21 16:05:20 +0530

    [N1vas] Refactor :  Data is appended

--- LinkedListProject/Program.cs
+++ LinkedListProject/Program.cs
@@ -6,9 +6,9 @@ namespace LinkedListProject
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
             list.Add(70);
-            list.AddFirst(30);
-            list.AddFirst(56);
             list.Display();
         }
     }
